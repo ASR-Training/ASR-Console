4b621e4162418f44e9cf872dccf1f7902c2a93d0
Farooq Muhammad <farooq@americansoftwareresources.com>
2018-09-29 03:41:06 +0530

    Added Addition Function to Basic Programing Class.

--- TestApplicationSession1/TestApplicationSession1/Program.cs
+++ TestApplicationSession1/TestApplicationSession1/Program.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TestApplicationSession1.sravanthi;
 namespace TestApplicationSession1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int result = basicprograming.Addition(20, 30);
+            Console.WriteLine("Addtion of 20 and 30 is {0}", result);
             Console.ReadKey();
         }
     }
--- TestApplicationSession1/TestApplicationSession1/sravanthi/basicprograming.cs
+++ TestApplicationSession1/TestApplicationSession1/sravanthi/basicprograming.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 
 namespace TestApplicationSession1.sravanthi
 {
-
-
     public class basicprograming
     {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
